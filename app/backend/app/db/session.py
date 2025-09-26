1ada4366b6fcb4ecb70125804a88d500fa4858b6
vdmkkk <soloviov.vad@gmail.com>
2025-09-26 12:28:38 +0300

    fix asyncio and celery

--- app/backend/app/db/session.py
+++ app/backend/app/db/session.py
@@ -1,15 +1,22 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from contextlib import asynccontextmanager
-
+from sqlalchemy.pool import NullPool
 from app.core.config import settings
+import os
 
 DATABASE_URL = (
     f"postgresql+asyncpg://{settings.PG_USER}:{settings.PG_PASSWORD}"
     f"@{settings.PG_HOST}:{settings.PG_PORT}/{settings.PG_DB}"
 )
 
-engine = create_async_engine(DATABASE_URL, echo=False, pool_pre_ping=True)
+DISABLE_ASYNC_POOL = os.getenv("IN_CELERY", "0") == "1" or os.getenv("DISABLE_ASYNC_DB_POOL", "0") == "1"
+
+engine_kwargs = dict(echo=False, pool_pre_ping=True)
+if DISABLE_ASYNC_POOL:
+    engine_kwargs["poolclass"] = NullPool
+
+engine = create_async_engine(DATABASE_URL, **engine_kwargs)
 
 AsyncSessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 
